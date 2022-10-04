cf7e7a9f83cdb068206576ee591bc3a9980f2adc
Ostap Ilkiv <ostap.ilkiv3408@gmail.com>
2022-10-04 18:18:26 +0300

    Lab3

---
+++ Lab_3/PlaneApp.java
@@ -0,0 +1,39 @@
+/**
+ * lab 3 package
+ */
+package Lab_3;
+
+import static java.lang.System.out;
+import java.io.*;
+
+
+/**
+ * Plane Application class implements main method for Plane class possibilities demonstration
+ * @author Ostap Ilkiv
+ * @version 1.0
+ */
+
+public class PlaneApp {
+    /**
+     * @param args
+     * @throws FileNotFoundException
+     */
+
+    public static void main(String[] args)throws FileNotFoundException {
+
+
+Plane plane1 = new Plane(2,3,4);
+        Plane plane2 = new Plane();
+        Plane plane3 = new Plane();
+
+        out.println(Plane.count);
+//plane1.info();
+//
+//plane1.go();
+//
+//        plane1.planeStop();
+
+
+
+    }
+}
